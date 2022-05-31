0cdf8551a4355b9ae757e83cee875fcaac4c2433
Micheal Reilly <info@educatedmachine.com>
2022-05-31 11:55:42 +0100

    added sample button

--- apps/native/src/Button.tsx
+++ apps/native/src/Button.tsx
@@ -1,17 +1,15 @@
-import { StatusBar } from "expo-status-bar";
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
 
-const App = () => {
+const Button = () => {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>Button</Text>
     </View>
   );
 };
 
-export default App;
+export default Button;
 
 const styles = StyleSheet.create({
   container: {
