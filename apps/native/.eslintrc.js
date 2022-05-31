3fe6c176c9633523088356ad9c23b77bc21bc621
Micheal Reilly <info@educatedmachine.com>
2022-05-31 12:11:04 +0100

    updated linting pathway and setup linting in ui-native

--- apps/native/.eslintrc.js
+++ apps/native/.eslintrc.js
@@ -1,9 +1,4 @@
 module.exports = {
   root: true,
   extends: ["../../packages/config/eslint-rn-native"],
-  parserOptions: {
-    root: true,
-    tsconfigRootDir: __dirname,
-    project: ["./tsconfig.json"],
-  },
 };
